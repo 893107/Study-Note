9db54e1c5d71386e076e9fb6336f0f88838c64cc
893107 <105209468+893107@users.noreply.github.com>
2022-09-28 11:55:37 +0900

    Create 짝수와홀수.java

---
+++ 코딩테스트/짝수와홀수.java
@@ -0,0 +1,12 @@
+
+# 프로그래머스 짝수와 홀수
+
+class Solution {
+    public String solution(int num) {
+        String answer = "Odd";
+        if(num % 2 == 0){
+            answer = "Even";
+        }
+        return answer;
+    }
+}
